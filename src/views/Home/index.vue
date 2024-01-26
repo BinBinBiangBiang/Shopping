c5549fd6539ab7f530c5d29d276af76e9ff8d65c
BinBinBiangBiang <3122583749@qq.com>
2024-01-26 17:39:33 +0800

    home-面板组件的封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,7 +6,15 @@
   </div>
     <HomeNew />
     <HomeHot />
-    <homeProduct />
+    <HomeProduct />
+    <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+        <div>
+        我是新鲜好物的插槽
+        </div>
+    </HomePanel>
+    <HomePanel title="新鲜好物">
+
+</HomePanel>
   </div>
 </template>
 
@@ -15,7 +23,10 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
+
+
 </script>
 
 <style lang="scss" scoped>
